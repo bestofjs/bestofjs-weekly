62571015d5de7c70b830b408ed033256d8e7e958
michaelrambeau <mikeair@gmail.com>
2018-12-24 07:27:32 +0900

    Fix SSR styles using `styled-components` plugin

--- static.config.js
+++ static.config.js
@@ -1,17 +1,13 @@
-import axios from 'axios'
-import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
+import React from 'react'
 
 import fetchContent from './src/utils/fetch-content'
 
 export default {
+  plugins: ['react-static-plugin-styled-components'],
   getSiteData: () => ({
-    title: 'React Static'
+    title: 'Best of JavaScript Weekly'
   }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
     const issues = await fetchContent()
     const latestIssue = issues[0]
     const getIssueRoute = issue => ({
@@ -56,46 +52,33 @@ export default {
       { path: '/latest', ...getIssueRoute(latestIssue) }
     ]
   },
-  renderToHtml: (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet()
-    const html = render(sheet.collectStyles(Comp))
-    meta.styleTags = sheet.getStyleElement()
-    return html
-  },
-  Document: class CustomHtml extends Component {
-    render() {
-      const { Html, Head, Body, children, renderMeta } = this.props
-      const ga = `
+  Document: ({ Html, Head, Body, children }) => {
+    const ga = `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'UA-44563970-5');      
       `
-      return (
-        <Html>
-          <Head>
-            <meta charSet="UTF-8" />
-            <title>Best of JavaScript Weekly</title>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            {renderMeta.styleTags}
-            <link
-              href="https://fonts.googleapis.com/css?family=Space+Mono:400,400i|Roboto+Slab:300,400,700"
-              rel="stylesheet"
-            />
-          </Head>
-          <Body>
-            {children}
-            <script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=UA-44563970-5"
-            />
-            <script dangerouslySetInnerHTML={{ __html: ga }} />
-          </Body>
-        </Html>
-      )
-    }
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <title>Best of JavaScript Weekly</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Space+Mono:400,400i|Roboto+Slab:300,400,700"
+            rel="stylesheet"
+          />
+        </Head>
+        <Body>
+          {children}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-44563970-5"
+          />
+          <script dangerouslySetInnerHTML={{ __html: ga }} />
+        </Body>
+      </Html>
+    )
   }
 }
