999f24cdaff88737ec48297aee814db52b09738a
michaelrambeau <mikeair@gmail.com>
2019-05-12 15:42:07 +0900

    Logging

--- src/utils/fetch-stories.js
+++ src/utils/fetch-stories.js
@@ -3,6 +3,7 @@ import path from 'path'
 
 export default async function fetchStories() {
   const folderPath = path.resolve(process.cwd(), 'data')
+  console.log('Reading stories data from', folderPath)
   const json = await jdown(folderPath)
   return json.stories
 }
