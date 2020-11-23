88e8e09daa746a03f1baddd4d53f4d3e17f0a255
michaelrambeau <michael@medmain.com>
2020-11-23 11:09:40 +0900

    Add missing card image to share links on social medias

--- static.config.js
+++ static.config.js
@@ -5,56 +5,56 @@ import fetchContent from './src/utils/fetch-content'
 async function getRoutes() {
   const issues = await fetchContent()
   const latestIssue = issues[0]
-  const getIssueRoute = issue => ({
+  const getIssueRoute = (issue) => ({
     template: 'src/containers/IssuePage',
     getData: () => ({
       issue,
-      isLatest: issue.number === latestIssue.number
-    })
+      isLatest: issue.number === latestIssue.number,
+    }),
   })
   return [
     {
       path: '/',
       template: 'src/containers/HomePage',
       getData: () => ({
-        issues
-      })
+        issues,
+      }),
     },
     {
       path: '/issues',
       template: 'src/containers/IssueListPage',
       getData: () => ({
-        issues
+        issues,
       }),
-      children: issues.map(issue => ({
+      children: issues.map((issue) => ({
         ...getIssueRoute(issue),
-        path: `/${issue.number}`
-      }))
+        path: `/${issue.number}`,
+      })),
     },
     { path: 'check-email', template: 'src/containers/CheckEmailPage' },
     {
       path: 'email-confirmed',
-      template: 'src/containers/EmailConfirmedPage'
+      template: 'src/containers/EmailConfirmedPage',
     },
     {
       path: 'existing-contact',
-      template: 'src/containers/ExistingContactPage'
+      template: 'src/containers/ExistingContactPage',
     },
     {
       path: '404',
-      template: 'src/containers/404'
+      template: 'src/containers/404',
     },
-    { path: '/latest', ...getIssueRoute(latestIssue) }
+    { path: '/latest', ...getIssueRoute(latestIssue) },
   ]
 }
 
 export default {
   plugins: [
     'react-static-plugin-styled-components',
-    'react-static-plugin-reach-router'
+    'react-static-plugin-reach-router',
   ],
   getSiteData: () => ({
-    title: 'Best of JavaScript Weekly'
+    title: 'Best of JavaScript Weekly',
   }),
   getRoutes: async () => {
     try {
@@ -76,8 +76,12 @@ export default {
       <Html>
         <Head>
           <meta charSet="UTF-8" />
-          <title>Best of JavaScript Weekly</title>
+          <title>Best of JS Weekly</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            property="og:image"
+            content="https://bestofjs.org/images/logo.png"
+          />
           <link
             href="https://fonts.googleapis.com/css?family=Space+Mono:400,400i|Roboto+Slab:300,400,700"
             rel="stylesheet"
@@ -93,5 +97,5 @@ export default {
         </Body>
       </Html>
     )
-  }
+  },
 }
