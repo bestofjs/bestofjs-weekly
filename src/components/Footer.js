7926ef308aebcbd6f7742f32d2548cadf0f0d9ed
michaelrambeau <michael@medmain.com>
2020-11-29 16:57:22 +0900

    Fix footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Div = styled.div`
-  margin-top: 2rem;
-  padding: 2rem 0;
-  border-top: 2px dashed #ffae63;
-`
-
-const P = styled.p`
-  margin-top: 0;
-  margin-bottom: 1rem;
-  &:last-child {
-    margin-bottom: 0;
-  }
-`
-
 const Footer = () => {
   return (
-    <Div>
-      <P>
+    <Container>
+      <p>
         Any question or feedback? Reach us on{' '}
         <a className="link" href="https://github.com/bestofjs/bestofjs-weekly">
           GitHub
-        </a>.
-      </P>
-      <P>
+        </a>
+        .
+      </p>
+      <p>
         <a className="link" href="https://bestofjs.org">
           Best of JavaScript
         </a>{' '}
         is a project created by Michael Rambeau, in Osaka, Japan.
-      </P>
-    </Div>
+      </p>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  margin-top: 2rem;
+  padding: 2rem 0;
+  border-top: 2px dashed #ffae63;
+  p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`
 
 export default Footer
